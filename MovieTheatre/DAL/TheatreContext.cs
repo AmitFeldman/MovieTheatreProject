ecfcaf13357224ba78febef319f61d22cae69a19
AmitF <amit123feldman@cs.colman.ac.il>
2019-06-16 19:55:22 +0300

    Add project files.

---
+++ MovieTheatre/DAL/TheatreContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using MovieTheatre.Models;
+
+
+namespace MovieTheatre.DAL
+{
+    public class Context : DbContext
+    {
+
+        public Context() : base("TheatreContext")
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MovieTheatre/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieTheatre.MvcApplication" Language="C#" %>
