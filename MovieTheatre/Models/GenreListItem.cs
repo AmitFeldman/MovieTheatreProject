c6f9cbfc9ccea77266391936bbfac5ca0a3f0546
Korteraz <razkorte@gmail.com>
2019-07-27 23:02:25 +0300

    Added requested graph with data from db

---
+++ MovieTheatre/Models/GenreListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieTheatre.Models
+{
+    public class GenreListItem
+    {
+        public string label { get; set; }
+        public int value { get; set; }
+        public string color { get; set; }
+    }
+}
