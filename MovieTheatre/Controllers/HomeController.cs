ac94e23ca692b48f431b601831883b08ef12ff4c
hanuniadir <hanuniadir@gmail.com>
2019-07-27 21:28:35 +0300

    start with home page, without photos

--- MovieTheatre/Controllers/HomeController.cs
+++ MovieTheatre/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieTheatre.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,9 @@ namespace MovieTheatre.Controllers
 {
     public class HomeController : Controller
     {
+        private Context db = new Context();
+        public string[] Posters { get; set; }
+
         public ActionResult Index()
         {
             // System.Web.Security.FormsAuthentication.SetAuthCookie("54", false);
@@ -15,6 +19,33 @@ namespace MovieTheatre.Controllers
             //HttpContext.Current.Session["userId"] = 5;
             Session.Add("CurrentUser", 54);
             int userid = (int)Session["CurrentUser"];
+
+
+            var tableBuilder = "";
+            var columns = 3;
+            var data = db.Movies;
+            var movieList = data.ToList();
+
+            for (var index = 0; index < movieList.Count; index++)
+            {
+                var item = movieList[index];
+                if (index % columns == 0)
+                {
+                    if (index > 0) tableBuilder += "</tr>";
+                    tableBuilder += "<tr width=\"100%\">";
+                }
+
+                tableBuilder += "<td text-align=center>";
+                // tableBuilder += "<tr><img src="+ @Html.DisplayFor(modelItem => item.Poster +") height=\"250px\" width=\"250px\"></tr>";
+                tableBuilder += item.Name + "</br>";
+                tableBuilder += item.Year + "</br>";
+                tableBuilder += item.Genre + "</br></br></br></br>";
+                tableBuilder += "</td>";
+            }
+
+            tableBuilder += "</tr>";
+            ViewData["myTable"] = tableBuilder;
+
             return View();
         }
 
