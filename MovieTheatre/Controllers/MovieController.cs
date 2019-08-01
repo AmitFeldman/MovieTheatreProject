5dd618bf5713313aab435a611d83609e3c062677
Korteraz <razkorte@gmail.com>
2019-08-01 21:22:08 +0300

    Graphs

--- MovieTheatre/Controllers/MovieController.cs
+++ MovieTheatre/Controllers/MovieController.cs
@@ -52,11 +52,16 @@ namespace MovieTheatre.Controllers
             var genres = from m in db.Movies
                          group m by m.Genre into g
                          select new { Genre = g.Key, Amount = g.Count() };
+            //select new { Genre = g.Key, Amount = g.Count() };
 
             GenreListItem genreItem;
-            GenreListItem[] genreList = new GenreListItem[genres.Count()];
+            GenreListItem[] genreList = new GenreListItem[genres.Count()+1];
             var index = 0;
             Color randomColor;
+            genreItem = new GenreListItem();
+            genreItem.label = "Genre";
+            genreItem.value = 1;
+            genreList[index++] = genreItem;
             foreach (var genre in genres)
             {
                 genreItem = new GenreListItem();
@@ -64,12 +69,40 @@ namespace MovieTheatre.Controllers
                 genreItem.value = genre.Amount;
                 randomColor = new Color();
                 randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
-                genreItem.color = randomColor.Name;
+                //genreItem.color = randomColor.Name;
                 genreList[index++] = genreItem;
             }
-           ViewBag.genres = genreList;
+            ViewBag.genres = genreList;
 
+            string[] genres2 = new string[genres.Count()];
+            string[] genres3;
+            string str = "";
+            foreach (var genre in genres)
+            {
+                genres3 = new string[2];
+                genres3[0] = genre.Genre;
+                genres3[1] = genre.Amount.ToString();
+                //genres2[index++] = genres3;
+                str = str + "[ " + genre.Genre.ToString() + "," + genre.Amount + " ],";
+            }
+            //ViewBag.genres = genres2;
+            str = str.Substring(0,str.Length-1);
+            //ViewBag.genres = str;
+
+            //ViewBag.genres = genreList;
+            HigherRankedMovies();
             return View(db.Movies.ToList());
+        }
+
+        // Generate the 10 higher rank movies
+        public void HigherRankedMovies()
+        {
+            var movies = (from m in db.Movies
+                          join r in db.Rating on m.ID equals r.MovieID
+                          group r by m.Name into g
+                          orderby g.Average(p => p.Stars) descending
+                          select new { indexLabel = g.Key, y = g.Average(p => p.Stars) }).Take(10);
+            ViewBag.movieRating = movies;
         }
 
         // GET: Movie/Details/5
@@ -92,21 +125,21 @@ namespace MovieTheatre.Controllers
             var client = new WebClient();
             Movie movie = new Movie();
             string httpString = "";
-           /* httpString = "http://www.omdbapi.com/?t=" +
-                                  name +
-                                  //  "&y=" + item.Year +
-                                  "&apikey=4c2cc9b2";
-            var json = client.DownloadString(httpString);
-            var data = (JObject)JsonConvert.DeserializeObject(json);
-            if (data["Response"].Value<string>() == "True")
-            {
-                movie.Name = data["Title"].Value<string>();
-                movie.Poster = data["Poster"].Value<string>();
-                movie.Description = data["Plot"].Value<string>();
-                movie.Director = data["Director"].Value<string>();
-                movie.Year = data["Year"].Value<string>();
-                movie.Genre = data["Genre"].Value<string>();
-            }*/
+            /* httpString = "http://www.omdbapi.com/?t=" +
+                                   name +
+                                   //  "&y=" + item.Year +
+                                   "&apikey=4c2cc9b2";
+             var json = client.DownloadString(httpString);
+             var data = (JObject)JsonConvert.DeserializeObject(json);
+             if (data["Response"].Value<string>() == "True")
+             {
+                 movie.Name = data["Title"].Value<string>();
+                 movie.Poster = data["Poster"].Value<string>();
+                 movie.Description = data["Plot"].Value<string>();
+                 movie.Director = data["Director"].Value<string>();
+                 movie.Year = data["Year"].Value<string>();
+                 movie.Genre = data["Genre"].Value<string>();
+             }*/
             return RedirectToAction("Create", movie);
         }
 
